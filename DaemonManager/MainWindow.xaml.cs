e0ff3f38f43330485d7830fbd1855b21d9805c9a
Andrew Sampson <andrew@codeusa523.org>
2016-12-03 21:24:51 -0500

    Push the source. Wrote in a single bound.

---
+++ DaemonManager/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DaemonManager.Tools;
+
+namespace DaemonManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var service = new Task(SetText);
+            service.Start();
+        }
+
+        private void SetText()
+        {
+            while (true)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    UlteriusInstalledLabel.Content = Utils.UlteriusInstalled() ? "Ulterius is Installed" : "Ulterius is Not Installed";
+                    ServerStatusBlock.Text = $"Server: {GetStatusString(Utils.ServerRunning())}";
+                    AgentStatusBlock.Text = $"Agent: {GetStatusString(Utils.AgentRunning())}";
+                    StateButton.Content = Utils.ServerRunning() ? "Kill Ulterius" : "Start Ulterius";
+                });
+
+                Thread.Sleep(1000);
+            }
+          
+        }
+
+        private string GetStatusString(bool status)
+        {
+            return status ? "Running" : "Stopped";
+        }
+
+        private bool ShowMissingError()
+        {
+            if (Utils.UlteriusInstalled()) return false;
+            MessageBox.Show("Ulterius is not currently installed.", "Uh Oh", MessageBoxButton.OK, MessageBoxImage.Information);
+            return true;
+        }
+        private void StateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ShowMissingError()) return;
+            if (Utils.ServerRunning() || Utils.AgentRunning())
+            {
+                Utils.KillUlterius();
+                MessageBox.Show("Ulterius has been stopped", "Sucess", MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            else
+            {
+                Utils.RestartService();
+                MessageBox.Show("Ulterius has been restarted", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ShowMissingError()) return;
+            Utils.RestartService();
+            MessageBox.Show("Ulterius has been restarted", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ DaemonManager/Tools/Utils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DaemonManager.Tools
+{
+    public static class Utils
+    {
+        public static string UlteriusPath = System.Environment.GetEnvironmentVariable("ulteriuspath", EnvironmentVariableTarget.Machine);
+        public static bool UlteriusInstalled()
+        {
+            if (string.IsNullOrEmpty(UlteriusPath))
+            {
+                return false;
+            }
+            var ulteriusDirectory = new DirectoryInfo(UlteriusPath);
+            var files = ulteriusDirectory.GetFiles("*.exe");
+            return files.Any(file => file.Name.Contains("Ulterius Server"));
+        }
+
+        public static bool ServerRunning()
+        {
+            return Process.GetProcessesByName("Ulterius Server").Length > 0;
+        }
+
+
+        public static bool AgentRunning()
+        {
+
+            return Process.GetProcessesByName("UlteriusAgent").Length > 0;
+        }
+
+        public static void KillUlterius()
+        {
+            var agentList = Process.GetProcessesByName("UlteriusAgent");
+            foreach (var agent in agentList)
+            {
+                try
+                {
+                    agent.Kill();
+                    agent.WaitForExit();
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+            var serverInstanceList = Process.GetProcessesByName("Ulterius Server");
+            foreach (var server in serverInstanceList)
+            {
+                try
+                {
+                    server.Kill();
+                    server.WaitForExit();
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+           
+        }
+        public static bool RestartService()
+        {
+            if (!UlteriusInstalled()) return false;
+            var restartServiceScript = Path.Combine(UlteriusPath,
+                "restartservice.bat");
+            var serviceRestartInfo = new ProcessStartInfo(restartServiceScript)
+            {
+                WindowStyle = ProcessWindowStyle.Minimized
+            };
+
+            var process = Process.Start(serviceRestartInfo);
+            if (process == null) return false;
+            process.WaitForExit();
+            return true;
+        }
+    }
+}
